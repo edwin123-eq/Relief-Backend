077b5b6a853dab7f1a99d85d5f843de728c72302
edwin123-eq <edwin.eqsoft1@gmail.com>
2025-04-07 16:46:23 +0530

    hi

--- obj/Debug/net6.0/ReliefApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/ReliefApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReliefApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e312f7a4a11cbd430c4bf4506a5a12b12715cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReliefApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReliefApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
